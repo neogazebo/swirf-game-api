e7378fdb96106f3baec171de4a6d74d226b5d46e
Ridwan Effendi <ridef86@gmail.com>
2017-05-24 15:27:17 +0700

    Register Device : penyusuaian dengan game API Request / Response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -93,7 +93,7 @@ $app->routeMiddleware([
 // $app->register(App\Providers\EventServiceProvider::class);
 $app->register(App\Providers\SwirfServiceProvider::class);
 
- $app->register(Illuminate\Redis\RedisServiceProvider::class);
+$app->register(Illuminate\Redis\RedisServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
