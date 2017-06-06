58f8779b2b32ac3b8c15f09434816f0bbd55f236
febri pratama <neogazebo@gmail.com>
2017-06-06 14:03:14 +0700

    refactor collectible items tobe ready for redis implementations

--- app/Http/Controllers/V1/ItemController.php
+++ app/Http/Controllers/V1/ItemController.php
@@ -67,74 +67,18 @@ class ItemController extends Controller {
 
     public function collectedItem()
     {
-	$statement = '
-		select 
-		coc_id as collected_id,
-		coc_collection_id as collection_id,
-		clc_name as collection_name,
-		clc_description as collection_description,
-		clc_start_date as collection_start_date,
-		clc_end_date as collection_end_date,
-		coc_datetime as collected_creation_date,
-		coc_flag as collected_flag,
-		coc_completed_datetime as collected_completed_date,
-		coc_redeemed_datetime as collected_redeemed_date,
-		clc_partner_id as partner_id,
-		clc_status as collection_status,
-		par_name as partner_name
-		from tbl_collectible_collection
-		inner join tbl_collection on coc_collection_id=clc_id
-		left join tbl_partner on clc_partner_id=par_id
-		where coc_member_id=:mem_id
-	';
+	$collections = $this->__getCollectedItems(\Swirf::getMember()->mem_id);
 
-	$collections = \DB::select($statement, [
-	    'mem_id' => \Swirf::getMember()->mem_id,
-	]);
-	
-	
-	
 	if (count($collections) <> 0)
 	{
-	    foreach ($collections as $collection) {
-		$statement = '
-			select 
-			col_id as collected_item_id,
-			col_datetime as collected_item_date,
-			itm_name as item_name, 
-			itm_point_value as item_point_value,
-			itm_rarity as rarity,
-			itm_image as image
-			from tbl_collectible
-			left join tbl_item on col_item_id=itm_id
-			where col_member_id=:mem_id and col_collection_id=:clc_id
-		';
-
-		$items = \DB::select($statement, [
-			    'mem_id' => \Swirf::getMember()->mem_id,
-			    'clc_id' => $collection->collection_id,
-		]);
-		$collection->items = $items;
-
-	    }
-	    
-	    if (count($items) <> 0)
-	    {
-		$this->code = CC::RESPONSE_SUCCESS;
-		$this->results = $collections;
-		$this->message = "Successful pulling the collected items.";
-	    }
-	    else
-	    {
-		$this->code = CC::RESPONSE_SUCCESS;
-		$this->results = $collections;
-		$this->message = "No collected items, please try to grab an item first.";
-	    }
+	    $this->code = CC::RESPONSE_SUCCESS;
+	    $this->results = $collections;
+	    $this->message = "Successful pulling the collected items.";
 	}
 	else
 	{
 	    $this->code = CC::RESPONSE_SUCCESS;
-	    $this->results = ['collected' => $collected];
+	    $this->results = $collections;
 	    $this->message = "No collection, please try to grab an item first.";
 	}
 
@@ -313,6 +257,61 @@ class ItemController extends Controller {
 	    $this->message = 'Error Parameters';
 	}
 	return $this->json();
+    }
+
+    private function __getCollectedItems($member_id)
+    {
+	//TODO get from Redis
+	$statement = '
+		select 
+		coc_id as collected_id,
+		coc_collection_id as collection_id,
+		clc_name as collection_name,
+		clc_description as collection_description,
+		clc_start_date as collection_start_date,
+		clc_end_date as collection_end_date,
+		coc_datetime as collected_creation_date,
+		coc_flag as collected_flag,
+		coc_completed_datetime as collected_completed_date,
+		coc_redeemed_datetime as collected_redeemed_date,
+		clc_partner_id as partner_id,
+		clc_status as collection_status,
+		par_name as partner_name
+		from tbl_collectible_collection
+		inner join tbl_collection on coc_collection_id=clc_id
+		left join tbl_partner on clc_partner_id=par_id
+		where coc_member_id=:mem_id
+	';
+
+	$collections = \DB::select($statement, [
+		    'mem_id' => $member_id,
+	]);
+
+	if (count($collections) <> 0)
+	{
+	    foreach ($collections as $collection) {
+		$statement = '
+			select 
+			col_id as collected_item_id,
+			col_datetime as collected_item_date,
+			itm_name as item_name, 
+			itm_point_value as item_point_value,
+			itm_rarity as rarity,
+			itm_image as image
+			from tbl_collectible
+			left join tbl_item on col_item_id=itm_id
+			where col_member_id=:mem_id and col_collection_id=:clc_id
+		';
+
+		$items = \DB::select($statement, [
+			    'mem_id' => \Swirf::getMember()->mem_id,
+			    'clc_id' => $collection->collection_id,
+		]);
+		$collection->items = $items;
+	    }
+	}
+
+	return $collections;
     }
 
 }
